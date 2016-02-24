ecb0f86a8d5db26253a29ad4d3e8a4e5a1d0ffc1
Rowan Thorpe <rowan@rowanthorpe.com>
2016-02-25 00:43:28 +0200

    Add optional debugging output

--- jpegconcat.sh
+++ jpegconcat.sh
@@ -21,6 +21,7 @@ scriptname=`basename "$0"`
 vertmode=0
 jpegtran_exec='./jpegtran'
 outfile='/dev/stdout'
+debug=0
 
 usage() {
     cat <<EOH
@@ -39,6 +40,7 @@ OPTIONS
  -V : concatenate vertically `test 0 -eq $vertmode || printf '(default)'`
  -e : jpegtran executable (default: ${jpegtran_exec})
  -o : output file (default: ${outfile})
+ -d : debugging output (default: `if test 1 -eq $debug; then printf 'on'; else printf 'off'; fi`)
 EOH
 }
 
@@ -52,6 +54,8 @@ die_u() {
     printf '\n' >&2
     die "$@"
 }
+
+dbgprint() { test 1 -ne $debug || printf "$@" >&2; }
 
 while test $# -ne 0; do
     case "$1" in
@@ -75,6 +79,10 @@ while test $# -ne 0; do
             outfile="$2"
             shift 2
             ;;
+        -d)
+            debug=1
+            shift
+            ;;
         --)
             shift
             break
@@ -91,17 +99,21 @@ done
 trap 'rm -f "$bufferfile" "$pivotfile" 2>/dev/null' EXIT
 bufferfile=`mktemp` || die 'failed to create temp bufferfile\n'
 pivotfile=`mktemp` || die 'failed to create temp pivotfile\n'
+dbgprint 'created temp bufferfile and pivotfile\n'
 
 cp "$1" "$bufferfile"
 shift
+dbgprint 'copied first file to bufferfile\n'
 
 while test $# -ne 0; do
     orig_size=`file -b "$bufferfile" | sed -e 's/^.*, *\([^,]\+\),[^,]\+$/\1/'`
     orig_width=`printf '%s\n' "$orig_size" | cut -dx -f1`
     orig_height=`printf '%s\n' "$orig_size" | cut -dx -f2`
+    dbgprint 'orig_size: %s, orig_width: %s, orig_height: %s\n' $orig_size $orig_width $orig_height
     new_size=`file -b "$1" | sed -e 's/^.*, *\([^,]\+\),[^,]\+$/\1/'`
     new_width=`printf '%s\n' "$new_size" | cut -dx -f1`
     new_height=`printf '%s\n' "$new_size" | cut -dx -f2`
+    dbgprint 'new_size: %s, new_width: %s, new_height: %s\n' $new_size $new_width $new_height
 
     if test 1 -eq $vertmode; then
         if test $orig_width -gt $new_width; then
@@ -118,15 +130,21 @@ while test $# -ne 0; do
         fi
         final_width=`expr $orig_width + $new_width`
     fi
+    dbgprint 'final_width: %s, final_height: %s\n' $final_width $final_height
     cropsize=${final_width}x$final_height
+    dbgprint 'cropsize: %s\n' $cropsize
     if test 1 -eq $vertmode; then
         dropsize=+0+$orig_height
     else
         dropsize=+${orig_width}+0
     fi
+    dbgprint 'dropsize: %s\n' $dropsize
 
     $jpegtran_exec -copy all -crop $cropsize -outfile "$pivotfile" "$bufferfile"
+    dbgprint '- executed expanding copy to pivotfile "%s"\n' "$pivotfile"
     $jpegtran_exec -copy all -drop $dropsize "$1" -outfile "$bufferfile" "$pivotfile"
+    dbgprint '- executed concatenating copy to bufferfile "%s"\n' "$bufferfile"
     shift
 done
 cat "$bufferfile" >"$outfile"
+dbgprint '- final file output to "%s"\n' "$outfile"
